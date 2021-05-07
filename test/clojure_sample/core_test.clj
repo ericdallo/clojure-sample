40f60e66ffd7b7b7c3af371482c59fa1ccadb541
Eric Dallo <ericdallo06@hotmail.com>
2021-05-07 19:21:02 -0300

    case

--- test/clojure_sample/core_test.clj
+++ test/clojure_sample/core_test.clj
@@ -1,20 +1,8 @@
 (ns clojure-sample.core-test
-  (:require [clojure-sample.core :refer :all]
-            [clojure.test :refer [deftest testing is]]
-            [matcher-combinators.test :refer [match?]]))
+  (:require [midje.sweet :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= {:a 2 :b 2}
-          {:a 2 :b 2}))))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= {:a 2 :b 2}
-          {:a 2 :b 2}))))
-
-(deftest matcher-test
-  (testing "testing matcher report"
-    (is (match?
-          {:bla 1 :blow {:f 2}}
-          {:blow {:bla {:a 2}}}))))
+(tabular
+  (fact "foo"
+    (+ ?a ?b) => ?res)
+  ?a ?b ?res
+  1  (+ 1 2)  3)
