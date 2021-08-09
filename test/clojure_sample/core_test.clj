b6c8ad8eda3c5a0668415e87df789b76c2952d97
Eric Dallo <ericdallo06@hotmail.com>
2021-08-08 23:31:05 -0300

    Add matcher combinatos

--- test/clojure_sample/core_test.clj
+++ test/clojure_sample/core_test.clj
@@ -1,14 +1,15 @@
 (ns clojure-sample.core-test
-  (:require [clojure-sample.core :refer :all]
-            [clojure.test :refer [deftest testing is]]
-            [matcher-combinators.test :refer [match?]]))
+  (:require
+   [clojure-sample.core :refer :all]
+   [clojure.test :refer [deftest is testing]]
+   [matcher-combinators.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= {:a 2 :b 2}
           {:a 2 :b 2}))))
 
-(deftest a-test
+(deftest b-test
   (testing "FIXME, I fail."
     (is (= {:a 2 :b 2}
           {:a 2 :b 2}))))
