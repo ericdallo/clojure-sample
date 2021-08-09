d035eaba67befbff3b12cd2e8b3b4cf6fd6c981c
Eric Dallo <ericdallo06@hotmail.com>
2021-08-08 23:44:27 -0300

    Add clojure-lsp

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot clojure-sample.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :clojure-lsp {:souce-paths ["src" "classes" "resources"]
+                           :dependencies [[com.github.clojure-lsp/clojure-lsp "2021.08.05-19.36.09"]]}}
+  :aliases {"clean-ns" ["with-profile" "+clojure-lsp" "run" "-m" "clojure-lsp.main" "clean-ns"]})
