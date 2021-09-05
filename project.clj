ca384fe3f1703bfb4a0cea32a3c8e103b39f78d7
Eric Dallo <ericdallo06@hotmail.com>
2021-09-04 22:52:10 -0300

    Update version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :plugins [[lein-clojure-lsp "0.1.0"]]
+  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.0.2"]]
   :main ^:skip-aot clojure-sample.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
