6488a7073d9d2145beed1a29413fa31b986186fa
Eric Dallo <ericdallo06@hotmail.com>
2021-11-20 16:55:14 -0300

    Bump clojure-lsp plugin

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :main ^:skip-aot clojure-sample.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.0.7"]]
+  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.1.3"]]
   :clojure-lsp {:settings {:clean {:ns-inner-blocks-indentation :same-line}}}
   :aliases {"clean-ns" ["clojure-lsp" "clean-ns" "--dry"]
             "format" ["clojure-lsp" "format" "--dry"]
