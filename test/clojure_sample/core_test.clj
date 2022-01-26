97902f8a2030f400a55155be928f08c1910de1cf
Eric Dallo <ericdallo06@hotmail.com>
2022-01-26 13:18:36 -0300

    Stateflow

--- test/clojure_sample/core_test.clj
+++ test/clojure_sample/core_test.clj
@@ -1,20 +1,19 @@
 (ns clojure-sample.core-test
   (:require
-   [clojure.test :refer [deftest is testing]]
-   [matcher-combinators.test :refer [match?]]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= {:a 2 :b 2}
-          {:a 2 :b 2}))))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= {:a 2 :b 2}
-          {:a 2 :b 2}))))
+   [clojure.test :as clojure.test :refer [deftest is testing]]
+   [matcher-combinators.parser]
+   [state-flow.assertions.matcher-combinators :refer [match?]]
+   ;; [matcher-combinators.test :refer [match?]]
+   [state-flow.api :refer [defflow flow]]))
 
 (deftest matcher-test
   (testing "testing matcher report"
     (is (match?
-          {:bla 1 :blow {:f 2}}
-          {:blow {:bla {:a 2}}}))))
+         {:bla 1 :blow {:f 2}}
+         {:blow {:bla {:a 2}}}))))
+
+(defflow matcher-test-bla
+  (flow "testing matcher report"
+        (match?
+         {:bla 1 :blow {:f 2}}
+         {:blow {:bla {:a (+ 1 2)}}})))
