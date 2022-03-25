4713fdc96e0d517f3d0f780124cf92c656fd0884
Eric Dallo <ericdallo06@hotmail.com>
2022-03-25 20:17:37 -0300

    Add java var-usages

--- src/clojure_sample/core.clj
+++ src/clojure_sample/core.clj
@@ -1,4 +1,9 @@
-(ns clojure-sample.core)
+(ns clojure-sample.core
+  (:import (clojure_sample Foo)
+           (clojure.lang PersistentVector)))
 
 (defn my-sum []
+  (Foo.)
+  (PersistentVector. 1 2)
+  (Thread/sleep 20)
   (+ 1 2))
